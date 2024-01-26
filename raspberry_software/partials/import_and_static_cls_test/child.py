46a30bf5acd04a1cad394e75ca88a8c1f6fcfa45
noblockhit <67117651+noblockhit@users.noreply.github.com>
2024-01-26 20:55:11 +0100

    initial commit

---
+++ raspberry_software/partials/import_and_static_cls_test/child.py
@@ -0,0 +1,5 @@
+from mdl import container
+
+
+def exec_smth():
+    container.attr2 = "two"
---
+++ raspberry_software/partials/import_and_static_cls_test/parent.py
@@ -0,0 +1,6 @@
+from mdl import container
+import child
+
+child.exec_smth()
+
+print(container.attr1, container.attr2)
